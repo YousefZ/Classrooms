bd9603e5a9bf45a195c775c25d181be4e8edf9c3
YousefZ <alzanki@gmail.com>
2018-07-24 16:07:29 +0300

    not final due to work contact

--- classes/forms.py
+++ classes/forms.py
@@ -5,3 +5,6 @@ class ClassroomForm(forms.ModelForm):
     class Meta:
         model = Classroom
         fields = '__all__'
+class UserLogin(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import UserLogin
+from django.contrib.auth import login, authenticate
 
 from .models import Classroom
 from .forms import ClassroomForm
@@ -56,3 +58,31 @@ def classroom_delete(request, classroom_id):
 	Classroom.objects.get(id=classroom_id).delete()
 	messages.success(request, "Successfully Deleted!")
 	return redirect('classroom-list')
+
+
+# user Login :
+def user_login(request):
+    form = UserLogin()
+    if request.method == 'POST':
+        form = UserLogin(request.POST)
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            auth_user = authenticate(username=username, password=password)
+            if auth_user is not None:
+                login(request, auth_user)
+                # Where you want to go after a successful login
+                return redirect('classroom-list')
+
+    context = {
+        "form":form
+    }
+    return render(request, 'login.html', context)
+
+#User Logout
+def user_logout(request):
+    logout(request)
+    # Where you would like to redirect the user after successfully logging out
+    return redirect("classroom-list")
